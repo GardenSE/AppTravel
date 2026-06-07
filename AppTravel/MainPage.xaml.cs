72fc28791e52e89e6b9105dae49d0c43ec5db428
GardenSE <t.t_garden.59@wine.plala.or.jp>
2026-06-07 17:13:42 +0900

    first commit

---
+++ AppTravel/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppTravel
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
